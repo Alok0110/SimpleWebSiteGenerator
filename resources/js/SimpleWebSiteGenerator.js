5153508fc7c1c2e15e0322089c4cc2e472d885a4
alok0110 <det.alex0110@gmail.com>
2019-09-01 13:11:33 +0530

    recent

---
+++ resources/js/SimpleWebSiteGenerator.js
@@ -0,0 +1,20 @@
+/* 
+ *
+ * Copyright (C) Joffer Systems, Inc - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alok Dethe <det.alex0110@gmail.com>, September 2019
+ *
+ *
+ *
+ */
+
+;(function(global){
+	"use strict"
+
+	/*
+
+	*/
+
+
+}(this));

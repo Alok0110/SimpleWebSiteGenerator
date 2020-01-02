a81402cee164c5282ea5e83b732b68cbba336aab
alok0110 <det.alex0110@gmail.com>
2020-01-02 19:33:51 +0530

    recent

--- resources/js/CalculateRecentIdAndMouse_min_.js
+++ resources/js/CalculateRecentIdAndMouse_min_.js
@@ -1,1 +1,11 @@
-!function(e,o){e.assignMOver=function(){for(var o=1;0!=e(".mover_"+o).length;)e(".mover_"+o).mouseover(function(o){var t=o.currentTarget.getAttribute("id");droppableElemArea=t,console.log("******** this is the elem that is calculated ******** "+t),console.log("executed mover"),e("#"+t).children(".COL_DRAG").each(function(o,t){e(t).removeClass("COL_DRAG"),e(t).addClass("COL_DRAG_R"),droppableElemInsideContainer=e(t).attr("id"),console.log("**** finalChildElem ****"),console.log(droppableElemInsideContainer)})}),e(".mover_"+o).mouseout(function(o){var t=o.currentTarget.getAttribute("id"),l=e("#"+t).children(".COL_DRAG_R");console.log("executed mout"),e("#drag_img_tp_1").removeAttr("tabindex"),l.each(function(o,t){e(t).removeClass("COL_DRAG_R"),e(t).addClass("COL_DRAG")})}),o++},e.calculateRecentIdForAll=function(o,t){var l="";"LO"===o?l=e("[id^="+t+"_"+o+"_]"):"TXT"===o&&(l=e("[id^="+t+"_"+o+"_]")),console.log(" ***** check calculateRecentIdForImage ***** "+t+"_"+o),console.log(l);var n,c,r=0,a=0,s="",i="";return e.each(l,function(o,t){if(s=e(t).attr("id"),"PID"!==(i=s.substr(s.length-3)))for(console.log("**** check PID ****"),console.log(i),n=s.lastIndexOf("_"),n-=1;"_"!==s.charAt(n);)r=parseInt(s.charAt(n)),a<r&&(a=r),n-=1,console.log("**** check cnt set ****"),console.log(a)}),c=t+"_"+o+"_"+(a+1)+"_ID",console.log(c),c}}(jQuery);
+/* 
+ *
+ * Copyright (C) Joffer Systems, Inc - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alok Dethe <det.alex0110@gmail.com>, September 2019
+ *
+ *
+ *
+ */
+!function(o,c){o.calculateRecentIdForAll=function(c,e){var l="";"LO"===c?l=o("[id^="+e+"_"+c+"_]"):"TXT"===c&&(l=o("[id^="+e+"_"+c+"_]")),console.log(" ***** check calculateRecentIdForImage ***** "+e+"_"+c),console.log(l);var n,t,a=0,r=0,s="",_="";return o.each(l,function(c,e){if(s=o(e).attr("id"),"PID"!==(_=s.substr(s.length-3)))for(console.log("**** check PID ****"),console.log(_),n=s.lastIndexOf("_"),n-=1;"_"!==s.charAt(n);)a=parseInt(s.charAt(n)),r<a&&(r=a),n-=1,console.log("**** check cnt set ****"),console.log(r)}),t=e+"_"+c+"_"+(r+1)+"_ID",console.log(t),t}}(jQuery);
